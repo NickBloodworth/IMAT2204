3fede7443cd0f854be699075b38eaca019c0d35f
Nick Bloodworth <p14181422@myemail.dmu.ac.uk>
2016-01-19 12:36:14 +0000

    Added test framework solution

---
+++ Cs4u/CS4u Test Framework/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CS4u_Test_Framework
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
