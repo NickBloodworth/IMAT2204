a7f4192eaee4146d9ceff06e5eab3b42fa1dc821
SallyBoulton <yellowbox@sky.com>
2016-02-02 15:59:11 +0000

    Orderline instance test

---
+++ Cs4u/CS4u Test Framework/tstOrderLine.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLibrary1;
+
+namespace CS4u_Test_Framework
+{
+    [TestClass]
+    public class tstOrderLine
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create an insnace of the class we want to create
+            clsOrderLine AnOrderLine= new clsOrderLine();
+            //test to dsee if it exists
+            Assert.IsNotNull(AnOrderLine);
+
+        }
+    }
+}
