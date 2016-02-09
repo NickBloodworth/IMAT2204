3f164feacab16d1addaf9ce14be3b4f4af28adf2
nickbloodworth <p14181422@myemail.dmu.ac.uk>
2016-02-09 12:28:07 +0000

    nick

--- Cs4u/CS4u Test Framework/tstOrderLine.cs
+++ Cs4u/CS4u Test Framework/tstOrderLine.cs
@@ -14,7 +14,21 @@ namespace CS4u_Test_Framework
             clsOrderLine AnOrderLine= new clsOrderLine();
             //test to dsee if it exists
             Assert.IsNotNull(AnOrderLine);
+         }
 
+        [TestMethod]
+        public void CompanyIDOK()
+        {
+            //create instance of class
+            clsOrderLine AnOrderLine = new clsOrderLine();
+            //create LineCost variable
+            double lineCost = 10.99;
+            //assign lineCost to AnOrderLine.LineCost
+            AnOrderLine.LineCost = lineCost;
+            //see if both variables are the same
+            Assert.AreEqual(AnOrderLine.LineCost, lineCost);
         }
+    [TestMethod]
+        public void 
     }
 }
