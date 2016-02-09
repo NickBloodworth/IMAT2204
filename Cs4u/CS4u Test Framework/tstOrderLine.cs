ba6c7a5a5463550b0ce893bb9cc662de825e8558
nickbloodworth <p14181422@myemail.dmu.ac.uk>
2016-02-09 15:53:25 +0000

    OrderLine

--- Cs4u/CS4u Test Framework/tstOrderLine.cs
+++ Cs4u/CS4u Test Framework/tstOrderLine.cs
@@ -11,10 +11,35 @@ namespace CS4u_Test_Framework
         public void InstanceOK()
         {
             //create an insnace of the class we want to create
-            clsOrderLine AnOrderLine= new clsOrderLine();
+            clsOrderLine AnOrderLine = new clsOrderLine();
             //test to dsee if it exists
             Assert.IsNotNull(AnOrderLine);
 
         }
+        [TestMethod]
+        public void OrderNoOK()
+        {
+            //create an insnace of the class we want to create
+            clsOrderLine AnOrderLine = new clsOrderLine();
+            //assign an orderNo property
+            Int32 OrderNo = 123;
+            //assign the data to the property
+            AnOrderLine.OrderNo = OrderNo;
+            //test to see if the to values are the same
+            Assert.AreEqual(AnOrderLine.OrderNo, OrderNo);
+        }
+        [TestMethod]
+        public void ValidMethodOK()
+        {
+            //create an insnace of the class we want to create
+            clsOrderLine AnOrderLine = new clsOrderLine();
+            Boolean OK = false;
+            //create some test data
+            string ItemCode = "item 1";
+            //invoke the method
+            OK = AnOrderLine.Valid(ItemCode);
+            Assert.IsTrue(OK);
+        }
     }
+
 }
